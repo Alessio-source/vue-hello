dbc2414eacf2433b3d9ee817bfcb5de6ab9be918
Alessio Pancia <alessiop2801@gmail.com>
2020-12-04 15:25:19 +0100

    script finito

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello world!',
+    img: '<img src="https://s1.1zoom.me/b5050/268/Mountains_Lake_Forests_458637_1920x1080.jpg"></img>'
+  }
+});
